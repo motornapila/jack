e072e8e21ea95580f4b3a99f7b11789a05591242
motornapila <motornapila@gmail.com>
2016-06-10 12:37:02 +0200

    started adding passport

--- app/models/User.js
+++ app/models/User.js
@@ -3,5 +3,8 @@ var mongoose = require('mongoose');
 var UserSchema = new mongoose.Schema({
 	username: String,
 	password: String,
+	department: String,
 	role: String
 });
+
+module.exports = mongoose.model('User', UserSchema);
--- server.js
+++ server.js
@@ -7,6 +7,10 @@ var port 			= process.env.PORT || 3000;
 var morgan 			= require('morgan');
 var bodyParser 		= require('body-parser');
 var methodOverride 	= require('method-override');
+var passport		= require('passport');
+var Strategy		= require('passport-local').Strategy;
+var expressSession	= require('express-session');
+var User            = require('./app/models/User.js');
 var app 			= express();
 var mongoUri		= process.env.MONGO_URI;
 
@@ -23,6 +27,36 @@ app.use(bodyParser.urlencoded({extended: true})); 	// parse application/x-www-fo
 app.use(bodyParser.text());							// allows bodyParser to look at raw text
 app.use(bodyParser.json({type: 'application/vnd.api+json'})); //parse application/vnd.api+json as json
 app.use(methodOverride());
+
+app.use(expressSession({
+	secret: 'scatman',
+	resave: false,
+	saveUninitialized: false
+}));
+
+// Passport config:
+app.use(new Strategy(function(username, password, cb){
+	User.findByUsername(username, function(err, user){
+		if(err) {return cb(err);}
+		if(!user) {return cb(null, false);}
+		if(user.password != password) { return cb(null, false);}
+		return cb(null, user);
+	});
+}));
+
+passport.serializeUser(function(user, cb) {
+	cb(null, user.id);
+});
+
+passport.deserializeUser(function(id, cb) {
+	User.findById(id, function(err, user) {
+		if(err) {return cb(err);}
+		cb(null, user);
+	});
+});
+
+app.use(passport.initialize());
+app.use(passport.session());
 
 //Routes
 require('./app/routes/routes.js')(app);
